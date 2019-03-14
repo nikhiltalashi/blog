feb3c2887686fcf80c07ba7008cdbb83cd61e88f
nikhiltalashi <wdnikhil@gmail.com>
2019-03-14 19:34:35 +0530

    Update customers.blade.php

--- resources/views/internals/customers.blade.php
+++ resources/views/internals/customers.blade.php
@@ -3,8 +3,8 @@
 @section('content')
     <h1>Customers</h1>
     <ul>
-      <li>John</li>
-      <li>Alex</li>
-      <li>Sam</li>
+        @foreach($customers as $customer)
+              <li>{{ $customer }}</li>
+        @endforeach
     </ul>
 @endsection
