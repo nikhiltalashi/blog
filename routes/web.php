59eae311d9677167878a97ec2b6001a3b3e2a2fd
nikhiltalashi <wdnikhil@gmail.com>
2019-03-14 17:19:46 +0530

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,25 @@
 <?php
+
 use App\Task;
 use Illuminate\Http\Request;
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
+/**
+ * Display All Tasks
+ */
 Route::get('/', function () {
     return view('tasks');
+});
+
+/**
+ * Add A New Task
+ */
+Route::post('/task', function (Request $request) {
+    //
+});
+
+/**
+ * Delete An Existing Task
+ */
+Route::delete('/task/{id}', function ($id) {
+    //
 });
