a29c0b61f7432ddaee7b8c2af5ad95b3b58c5dae
nikhiltalashi <wdnikhil@gmail.com>
2019-03-14 17:28:53 +0530

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,35 +1,8 @@
 <?php
 
-use App\Task;
-use Illuminate\Http\Request;
-
 /**
  * Display All Tasks
  */
 Route::get('/', function () {
     return view('tasks');
-});
-
-/**
- * Add A New Task
- */
-Route::post('/task', function (Request $request) {
-    $validator = Validator::make($request->all(), [
-        'name' => 'required|max:255',
-    ]);
-
-    if ($validator->fails()) {
-        return redirect('/')
-            ->withInput()
-            ->withErrors($validator);
-    }
-
-    // Create The Task...
-});
-
-/**
- * Delete An Existing Task
- */
-Route::delete('/task/{id}', function ($id) {
-    //
 });
