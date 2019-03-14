e0f2cdd49cf0f18cfcb001840b3fc641ed08d067
nikhiltalashi <wdnikhil@gmail.com>
2019-03-14 18:35:37 +0530

    Update customers.blade.php

--- resources/views/internals/customers.blade.php
+++ resources/views/internals/customers.blade.php
@@ -1,6 +1,10 @@
-<h1>Customers</h1>
-<ul>
-  </li>John</li>
-  </li>Alex</li>
-  </li>Sam</li>
-</ul>
+@extends('layouts.main')
+
+@section('content')
+    <h1>Customers</h1>
+    <ul>
+      </li>John</li>
+      </li>Alex</li>
+      </li>Sam</li>
+    </ul>
+@endsection
