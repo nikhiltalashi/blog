77391c341295febbbd04ff81be27be5460b47975
nikhiltalashi <wdnikhil@gmail.com>
2019-03-19 15:52:28 +0530

    Update footer.blade.php

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,3 +1,4 @@
-<footer id="footer" class="text text-center">
+<footer id="footer" class="text text-center" style="margin-top: 100px;">
+    </hr>
     <p>Copyright &copy Nikhil Talashi</p>
 </footer>
