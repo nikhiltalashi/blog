14af7621ac12dde91404c00bc9c9a5cc0c161b92
nikhiltalashi <wdnikhil@gmail.com>
2019-03-19 16:10:02 +0530

    Update CreatesController.php

--- app/Http/Controllers/CreatesController.php
+++ app/Http/Controllers/CreatesController.php
@@ -7,8 +7,13 @@ use Illuminate\Http\Request;
 class CreatesController extends Controller
 {
     public function home(){
+        
+        $articles = Article::all();
+        echo "<pre>";
+        print_r($articles);
+        echo "</pre>";
     
-        return view('home');
+        //return view('home');
         
     }
 }
