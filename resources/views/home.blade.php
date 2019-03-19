8ab40622f4d66fdb64efc382a00131781a5653cd
nikhiltalashi <wdnikhil@gmail.com>
2019-03-19 15:19:39 +0530

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,32 @@
-@extends('layouts.main')
+@include('layouts.header')
+<div class="container">
+    <div class="row">
+        <div class="col-md-6">
+            <legend>Laravel CRUD Application</legend>
+            <table class="table table-hover">
+              <thead>
+                <tr>
+                  <th scope="col">ID</th>
+                  <th scope="col">Title</th>
+                  <th scope="col">Description</th>
+                  <th scope="col">Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>Column content</td>
+                  <td>Column content</td>
+                  <td>Column content</td>
+                  <td>
+                    <a href="{{ url('') }}" class="label label-primary">Read</a> | 
+                    <a href="{{ url('') }}" class="label label-Success">Update</a> | 
+                    <a href="{{ url('') }}" class="label label-danger">Delete</a>
+                  </td>
+                </tr>
+               </tbody>
+            </table> 
+        </div>
+    </div>
+</div>
 
-@section('content')
-    <h1>Home</h1>
-@endsection
+@include('layouts.footer')
