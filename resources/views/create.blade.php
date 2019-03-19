3a8b88b86978a6bfccd1d36722b65d281423cde1
nikhiltalashi <wdnikhil@gmail.com>
2019-03-19 16:22:37 +0530

    Create create.blade.php

---
+++ resources/views/create.blade.php
@@ -0,0 +1,15 @@
+<form>
+  <fieldset>
+    <legend>Legend</legend>
+    <div class="form-group">
+      <label for="exampleTextarea">Example textarea</label>
+      <textarea class="form-control" id="exampleTextarea" rows="3"></textarea>
+    </div>
+    <div class="form-group">
+      <label for="exampleInputFile">File input</label>
+      <input type="file" class="form-control-file" id="exampleInputFile" aria-describedby="fileHelp">
+      <small id="fileHelp" class="form-text text-muted">This is some placeholder block-level help text for the above input. It's a bit lighter and easily wraps to a new line.</small>
+    </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
+  </fieldset>
+</form>
