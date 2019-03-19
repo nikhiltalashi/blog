8d6f797938ae22eb0626eec7bdf8ed1085ae2097
nikhiltalashi <wdnikhil@gmail.com>
2019-03-19 20:23:57 +0530

    Update read.blade.php

--- resources/views/read.blade.php
+++ resources/views/read.blade.php
@@ -3,8 +3,8 @@
     <div class="row">
         <div class="col-md-12">
             <legend>Read Article</legend>
-            {{ $articles->title }}
-            {{ $articles->description }}
+            <p>{{ $articles->title }}</p>
+            <p>{{ $articles->description }}</p>
         </div>
     </div>
 </div>
