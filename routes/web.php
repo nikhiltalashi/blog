f9ba5f9b79ca3cc33f3d774908a571def7c1d132
nikhiltalashi <wdnikhil@gmail.com>
2019-03-19 15:20:34 +0530

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -3,28 +3,3 @@
 Route::get('/', function () {
     return view('home');
 });
-
-Route::get('about', function () {
-    return view('about');
-});
-
-Route::get('contact', function () {
-    return view('contact');
-});
-
-Route::get('customers', function () {
-    
-    $customers = [
-        'John',
-        'Alex',
-        'Sam'
-    ];
-    
-    return view('internals.customers',[
-        'customers' => $customers
-    ]);
-});
-
-Route::get('/projects', 'ProjectsController@index');
-
-Route::post('/projects', 'ProjectsController@store');
