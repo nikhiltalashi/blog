8d4305fd8942adb984d1c2f8b938dd3acf75bf19
nikhiltalashi <wdnikhil@gmail.com>
2019-03-19 18:15:51 +0530

    Update CreatesController.php

--- app/Http/Controllers/CreatesController.php
+++ app/Http/Controllers/CreatesController.php
@@ -15,22 +15,23 @@ class CreatesController extends Controller
     }
         
     public function add(Request $request){
-       /* $this->validate($request,[
+        $this->validate($request,[
             'title' => 'required',
             'description' => 'required'
-        ]);*/
+        ]);
         
         
-        $data = $request->input();
+          if ($validator->fails()) {
+            return redirect('create')
+                        ->withErrors($validator)
+                        ->withInput();
+        }
         
-        echo "<pre>";
-        print_r($data);
-        echo "</pre>";
-            
+        
         
         //return view('create', ['datas' => $data]);
         
-        //return "Validation Pass";
+        return "Validation Pass";
         
     
     }
