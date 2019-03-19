d51c1111525ebd2a9d9ac4885e2bc8a10d6edf93
nikhiltalashi <wdnikhil@gmail.com>
2019-03-19 17:11:15 +0530

    Update create.blade.php

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -2,7 +2,7 @@
 <div class="container" style="margin-top : 25px">
     <div class="row">
         <div class="col-md-12">
-        <form>
+        <form method="POST" action="url('/insert')">
           <fieldset>
             <legend>New Article</legend>
             <div class="form-group">
